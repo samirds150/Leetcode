9ad7c9c3b3e7702df66ef50b47d886158b982c90
Sam <sameer.easyr@gmail.com>
2022-08-22 13:42:49 +0530

    added len of last word and search insert position

---
+++ Search Insert Position.py
@@ -0,0 +1,11 @@
+# search insert position
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+        return len(nums)
+
